ed6a09ad71bcd4d7e2a45062d1a0d500458b2d02
Kristoffer <krisose@stud.ntnu.no>
2024-10-07 13:57:26 +0200

    refactor: add basename

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -2,16 +2,21 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
 import SearchPage from './pages/Search.page';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+    {
+      path: '/search',
+      element: <SearchPage />,
+    },
+  ],
   {
-    path: '/project2',
-    element: <HomePage />,
-  },
-  {
-    path: '/project2/search',
-    element: <SearchPage />,
-  },
-]);
+    basename: '/project2',
+  }
+);
 
 export function Router() {
   return <RouterProvider router={router} />;
