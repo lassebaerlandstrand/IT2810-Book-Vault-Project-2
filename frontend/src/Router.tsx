09ca57b885695aba8eb4b5fcb7e5ba04deabded8
Lasse Bærland Strand <lassebs@stud.ntnu.no>
2024-10-07 15:16:24 +0200

    refactor: Change Mantine default setup values

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+  ],
   {
-    path: '/project2',
-    element: <HomePage />,
-  },
-]);
+    basename: '/project2',
+  }
+);
 
 export function Router() {
   return <RouterProvider router={router} />;
--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,5 +1,12 @@
 import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
-  /** Put your mantine theme override here */
+  fontFamily: 'Poppins, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji',
+  breakpoints: {
+    xs: '30em', // 480px
+    sm: '48em', // 768px
+    md: '64em', // 1024px
+    lg: '75em', // 1200px
+    xl: '90em', // 1440px
+  },
 });
