92bf6ffcd2ccf172e8ff7120e515044f1909ce7a
Lasse Bærland Strand <lassebs@stud.ntnu.no>
2024-10-09 11:50:56 +0200

    feat: Add pagination

--- frontend/src/pages/Home.page.tsx
+++ frontend/src/pages/Home.page.tsx
@@ -1,16 +1,29 @@
-import { fetchBooks } from '@/api/dummyApi';
+import { useEffect, useState } from 'react';
+import { Center, Pagination } from '@mantine/core';
+import { fetchBooks, fetchTotalBooksWithFilters } from '@/api/dummyApi';
 import BookCardGrid from '@/components/BookCardGrid/BookCardGrid';
+import { Book } from '@/generated/graphql';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
 
 export function HomePage() {
-  const books = fetchBooks().slice(0, 10);
+  const [page, setPage] = useState<number>(1);
+  const [limit, setLimit] = useState<number>(25);
+  const totalBooks = fetchTotalBooksWithFilters();
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    setBooks(fetchBooks(page, limit));
+  }, [page, limit]);
 
   return (
     <>
       <Welcome />
       <ColorSchemeToggle />
       <BookCardGrid books={books} />
+      <Center>
+        <Pagination total={Math.ceil(totalBooks / limit)} value={page} onChange={setPage} my="md" />
+      </Center>
     </>
   );
 }
