5bbcda554f2e3dd6845c97580e66e6fb575df382
Adam S <134907338+adam-sioud@users.noreply.github.com>
2024-10-11 11:31:27 +0200

    fix: tests are now passing

--- frontend/src/components/HeaderSimple/HeaderSimple.test.tsx
+++ frontend/src/components/HeaderSimple/HeaderSimple.test.tsx
@@ -1,59 +1,39 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@test-utils';
 import { MemoryRouter } from 'react-router-dom';
-import { HeaderSimple } from './HeaderSimple';
+import { HeaderSimple } from '@/components/HeaderSimple/HeaderSimple';
+import classes from './HeaderSimple.module.css';
 
 describe('HeaderSimple Component', () => {
-  test('renders all the links', () => {
+  test('renders navigation links correctly', () => {
     render(
       <MemoryRouter>
         <HeaderSimple />
       </MemoryRouter>
     );
 
-    const homeLink = screen.getByText(/HOME/i);
-    const booksLink = screen.getByText(/BOOKS/i);
+    const homeLink = screen.getByText(/home/i);
+    const booksLink = screen.getByText(/books/i);
 
     expect(homeLink).toBeInTheDocument();
     expect(booksLink).toBeInTheDocument();
   });
 
-  test('changes active link on click', () => {
+  test('updates active link on click', () => {
     render(
       <MemoryRouter>
         <HeaderSimple />
       </MemoryRouter>
     );
 
-    const homeLink = screen.getByText(/HOME/i);
-    const booksLink = screen.getByText(/BOOKS/i);
+    const homeLink = screen.getByText(/home/i);
+    const booksLink = screen.getByText(/books/i);
 
-    expect(homeLink).toHaveClass('active');
-    expect(booksLink).not.toHaveClass('active');
+    expect(homeLink).toHaveClass(classes.active);
+    expect(booksLink).not.toHaveClass(classes.active);
 
     fireEvent.click(booksLink);
 
-    expect(booksLink).toHaveClass('active');
-    expect(homeLink).not.toHaveClass('active');
-  });
-
-  test('toggles mobile menu on burger icon click', () => {
-    render(
-      <MemoryRouter>
-        <HeaderSimple />
-      </MemoryRouter>
-    );
-
-    const mobileMenu = screen.queryByText(/HOME/i);
-    expect(mobileMenu).not.toBeVisible();
-
-    const burgerIcon = screen.getByRole('button');
-    fireEvent.click(burgerIcon);
-
-    const openedMenu = screen.getByText(/HOME/i);
-    expect(openedMenu).toBeVisible();
-
-    fireEvent.click(burgerIcon);
-    const closedMenu = screen.queryByText(/HOME/i);
-    expect(closedMenu).not.toBeVisible();
+    expect(booksLink).toHaveClass(classes.active);
+    expect(homeLink).not.toHaveClass(classes.active);
   });
 });
