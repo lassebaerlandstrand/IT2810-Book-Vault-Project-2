b9f6a9c21fe9842b5b0f63d99ed509f32b9841b8
Lasse Bærland Strand <lassebaerlandstrand@gmail.com>
2024-10-16 22:34:41 +0200

    feat: Add more error pages

--- frontend/src/components/ErrorPage/ErrorPage.tsx
+++ frontend/src/components/ErrorPage/ErrorPage.tsx
@@ -1,5 +1,16 @@
-import { Link } from 'react-router-dom';
-import { Button, Container, Group, Text, Title } from '@mantine/core';
+import { isRouteErrorResponse, Link, useRouteError } from 'react-router-dom';
+import {
+  Button,
+  Center,
+  Code,
+  Collapse,
+  Container,
+  Group,
+  Stack,
+  Text,
+  Title,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { Illustration404 } from './Illustration404';
 import styles from './ErrorPage.module.css';
 
@@ -9,12 +20,59 @@ type ErrorPageProps = {
   link?: string;
 };
 
-// From ui.mantine.dev
-export function ErrorPage({
+export function ErrorPage({ title, description, link }: ErrorPageProps) {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error) && error.status === 404) {
+    return <Error404 title={title} description={description} link={link} />;
+  }
+
+  return <ErrorUnexpectedError error={error as Error} />;
+}
+
+const ErrorUnexpectedError = ({ error }: { error: Error }) => {
+  const [opened, { toggle }] = useDisclosure(true);
+
+  const errorMessage = error?.message || 'Unknown error';
+  const errorStack = error?.stack || 'No stack trace available';
+
+  return (
+    <Container className={styles.container}>
+      <Container className={styles.content}>
+        <Title className={styles.title}>Unexpected Application Error!</Title>
+        <Text c="dimmed" size="lg" ta="center" className={styles.description}>
+          Something went wrong, please try again.
+        </Text>
+        <Center>
+          <Link to={'/'}>
+            <Button size="md">Take me back to safe ground</Button>
+          </Link>
+        </Center>
+        <Center mt="xl">
+          <Stack align="center">
+            <Button onClick={toggle} w="fit-content">
+              {opened ? 'Hide' : 'Open'} error message
+            </Button>
+            <Collapse in={opened}>
+              <Code block mt="sm">
+                {errorMessage}
+              </Code>
+              <Code block mt="sm">
+                {errorStack}
+              </Code>
+            </Collapse>
+          </Stack>
+        </Center>
+      </Container>
+    </Container>
+  );
+};
+
+const Error404 = ({
   title = 'Nothing to see here',
   description = 'Page you are trying to open does not exist. You may have mistyped the address, or the page has been moved to another URL.',
   link = '/',
-}: ErrorPageProps) {
+}: ErrorPageProps) => {
   return (
     <Container className={styles.container}>
       <Illustration404 className={styles.image} />
@@ -31,4 +89,4 @@ export function ErrorPage({
       </Container>
     </Container>
   );
-}
+};
