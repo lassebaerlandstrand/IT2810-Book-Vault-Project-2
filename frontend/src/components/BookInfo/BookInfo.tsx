f1cf48146e3a3e2bec36d5c7219c45f44e3a5e64
Sebastian Munthe <sebasmu@stud.ntnu.no>
2024-10-21 23:34:34 +0200

    feat: Updated desktop design for bookpage to get it more ready for reviews

--- frontend/src/components/BookInfo/BookInfo.tsx
+++ frontend/src/components/BookInfo/BookInfo.tsx
@@ -1,6 +1,7 @@
-import { Container, Flex, Group, Image, Rating, Text } from '@mantine/core';
+import { Container, Flex, Grid, Group, Image, Rating, Text } from '@mantine/core';
 import { Book } from '@/generated/graphql';
 import InfoGrid from '../InfoGrid/InfoGrid';
+import styles from './BookInfo.module.css';
 
 type BookInfoProps = {
   book: Book;
@@ -9,7 +10,7 @@ type BookInfoProps = {
 const BookInfo = ({ book }: BookInfoProps) => {
   return (
     <Group justify="center" gap="lg">
-      <Container p="xs">
+      <Container p="xs" className={styles.titleContainer}>
         <Text size="lg" fw={700} component="h1">
           {book.title}
         </Text>
@@ -17,33 +18,41 @@ const BookInfo = ({ book }: BookInfoProps) => {
           {book.authors[0]} {book.authors.length > 1 ? 'et al.' : ''}
         </Text>
       </Container>
-      <Flex gap="sm" justify="center" align="center" direction="row" wrap="wrap">
-        <Image
-          src={book.coverImg}
-          alt={`Cover image for ${book.title}`}
-          fit="contain"
-          fallbackSrc="https://placehold.co/200x300?text=Cover%20image%20for%20book"
-          radius="lg"
-          sizes="xs"
-        />
-        <Flex gap="sm" justify="center" align="center" direction="row" wrap="wrap">
-          <Container p="xs" size="sm">
-            <Text>
-              {book.genres.slice(0, 3).join(', ')}
-              {book.genres.length > 3 ? '...' : null}
-            </Text>
+      <Grid justify="left" align="top">
+        <Grid.Col span={{ xs: 12, sm: 4, md: 4, lg: 4, xl: 4 }} h={400}>
+          <Image
+            src={book.coverImg}
+            alt={`Cover image for ${book.title}`}
+            fit="contain"
+            fallbackSrc="https://placehold.co/200x300?text=Cover%20image%20for%20book"
+            radius="lg"
+            sizes="xs"
+            w="fit-content"
+            maw="100%"
+            mah="100%"
+            m="auto"
+          />
+        </Grid.Col>
+        <Grid.Col span="auto">
+          <Flex gap="sm" justify="center" align="center" direction="row" wrap="wrap">
+            <Container p="xs" size="sm">
+              <Text>
+                {book.genres.slice(0, 3).join(', ')}
+                {book.genres.length > 3 ? '...' : null}
+              </Text>
 
-            <Flex justify="center" align="center" gap={7} mt="xs">
-              <Rating value={Math.round(book.rating * 2) / 2} fractions={2} readOnly />
-              <Text fw={500}>{book.rating.toFixed(1)}</Text>
-            </Flex>
-          </Container>
+              <Flex justify="center" align="center" gap={7} mt="xs">
+                <Rating value={Math.round(book.rating * 2) / 2} fractions={2} readOnly />
+                <Text fw={500}>{book.rating.toFixed(1)}</Text>
+              </Flex>
+            </Container>
 
-          <Text>{book.description}</Text>
-        </Flex>
+            <Text>{book.description}</Text>
+          </Flex>
+        </Grid.Col>
 
         <InfoGrid book={book} />
-      </Flex>
+      </Grid>
     </Group>
   );
 };
