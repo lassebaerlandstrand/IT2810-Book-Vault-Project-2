48be9d6c281bb8a45ec69eacb6c287bec003d089
Lasse Bærland Strand <lassebaerlandstrand@gmail.com>
2024-10-30 23:14:47 +0100

    feat: Add image version of logo

--- frontend/src/components/Logo/Logo.tsx
+++ frontend/src/components/Logo/Logo.tsx
@@ -2,6 +2,7 @@ import { SVGProps } from 'react';
 import { Group, GroupProps, Image, ImageProps } from '@mantine/core';
 import BookVaultLogo from '../../assets/BookVaultLogo.png';
 import BookVaultLogoWebP from '../../assets/BookVaultLogo.webp';
+import BookVaultTextImage from '../../assets/BookVaultText.svg';
 import BookVaultText from '../../assets/BookVaultText.svg?react';
 import styles from './Logo.module.css';
 
@@ -9,17 +10,25 @@ export const LogoFull = (props: GroupProps) => {
   return (
     <>
       <Group component="figure" {...props} className={`${styles.wrapper} ${props.className ?? ''}`}>
-        <Image src={BookVaultLogoWebP} fallbackSrc={BookVaultLogo} />
-        <BookVaultText />
+        <LogoIcon />
+        <LogoText />
       </Group>
     </>
   );
 };
 
 export const LogoIcon = (props: ImageProps) => {
-  return <Image src={BookVaultLogoWebP} fallbackSrc={BookVaultLogo} {...props} />;
+  return (
+    <Image src={BookVaultLogoWebP} alt="Book Logo Icon" fallbackSrc={BookVaultLogo} {...props} />
+  );
 };
 
+/** Returns the logo text as Vite SVG using svgr, faster to load compared to image. Is not highlightable */
 export const LogoText = (props: SVGProps<SVGSVGElement>) => {
   return <BookVaultText {...props} />;
+};
+
+/** Return the logo text as an image. This is useful if you want the text to be highlightable */
+export const LogoTextImage = (props: ImageProps) => {
+  return <Image src={BookVaultTextImage} {...props} />;
 };
