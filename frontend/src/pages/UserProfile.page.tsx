67e75572d579848dd48aaef93b6c62a9494a4134
Adam S <134907338+adam-sioud@users.noreply.github.com>
2024-11-01 11:37:37 +0100

    add: button to reviews

--- frontend/src/pages/UserProfile.page.tsx
+++ frontend/src/pages/UserProfile.page.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
-import { Avatar, Button, Container, Input, Text, Title } from '@mantine/core';
+import { Link } from 'react-router-dom';
+import { Avatar, Button, Container, Input, Stack, Text, Title } from '@mantine/core';
 import { useUser } from '@/contexts/UserFunctions';
+import { Review } from '@/generated/graphql';
 import { useYourBookReviews } from '@/hooks/useYourBookReviews';
 import ReviewStack from '../components/ReviewStack/ReviewStack';
 import styles from './Profile.module.css';
@@ -29,54 +31,65 @@ export function ProfilePage() {
 
   return (
     <Container size="sm" my="xl" className={styles.profileContainer}>
-      <Avatar size={100} radius="xl" className={styles.avatar}>
-        {newName
-          .split(' ')
-          .map((n) => n[0])
-          .join('')}
-      </Avatar>
-      <Text align="center" size="lg" weight={600} className={styles.userName}>
-        {user.name}
-      </Text>
-
-      {isEditing ? (
-        <>
-          <Input
-            value={newName}
-            onChange={(event) => setNewName(event.target.value)}
-            placeholder="Enter new name"
-            className={styles.input}
-            autoFocus
-          />
-          <Button fullWidth radius="md" onClick={handleNameChange} className={styles.saveButton}>
-            Save
-          </Button>
-        </>
-      ) : (
-        <Button
-          fullWidth
-          radius="md"
-          size="md"
-          variant="default"
-          onClick={() => setIsEditing(true)}
-          className={styles.editButton}
-        >
-          Edit Name
-        </Button>
-      )}
-
-      <Title order={2} className={styles.sectionTitle}>
-        Your Reviews
-      </Title>
-      {loading ? (
-        <Text align="center">Loading reviews...</Text>
-      ) : error ? (
-        <Text align="center" color="red">
-          Error fetching reviews
+      <Stack>
+        <Avatar size={100} radius="xl" className={styles.avatar}>
+          {newName
+            .split(' ')
+            .map((n) => n[0])
+            .join('')}
+        </Avatar>
+        <Text ta="center" size="lg" className={styles.userName}>
+          {user.name}
         </Text>
-      ) : (
-        <ReviewStack reviews={reviews} type="yourReview" />
-      )}
+
+        {isEditing ? (
+          <>
+            <Input
+              value={newName}
+              onChange={(event) => setNewName(event.target.value)}
+              placeholder="Enter new name"
+              className={styles.input}
+              autoFocus
+            />
+            <Button fullWidth radius="md" onClick={handleNameChange} className={styles.saveButton}>
+              Save
+            </Button>
+          </>
+        ) : (
+          <Button
+            fullWidth
+            radius="md"
+            size="md"
+            variant="default"
+            onClick={() => setIsEditing(true)}
+            className={styles.editButton}
+          >
+            Edit Name
+          </Button>
+        )}
+
+        <Title order={2} className={styles.sectionTitle}>
+          Your Reviews
+        </Title>
+        {loading ? (
+          <Text ta="center">Loading reviews...</Text>
+        ) : error ? (
+          <Text ta="center" c="red">
+            Error fetching reviews
+          </Text>
+        ) : (
+          <>
+            <ReviewStack reviews={reviews as Review[]} type="bookReview" />
+            {reviews && reviews.length >= 3 && (
+              <Link to="/myReviews" className={styles.viewMoreLink}>
+                <Button fullWidth radius="md" variant="outline">
+                  View All Reviews
+                </Button>
+              </Link>
+            )}
+          </>
+        )}
+      </Stack>
     </Container>
   );
 }
