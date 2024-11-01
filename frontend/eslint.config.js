d06e7cc44162606133d7446548fd748d5c19ecb8
Sebastian Munthe <sebasmu@stud.ntnu.no>
2024-11-01 08:36:46 +0100

    fix: fixed eslint config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,9 +1,8 @@
 import mantine from 'eslint-config-mantine';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(...mantine, {
-  ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', 'src/generated/'],
-  rules: {
-    'no-console': ['error', { allow: ['warn', 'error'] }],
-  },
-});
+export default tseslint.config(
+  ...mantine,
+  { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', 'src/generated/'] },
+  { rules: { 'no-console': ['error', { allow: ['warn', 'error'] }] } }
+);
