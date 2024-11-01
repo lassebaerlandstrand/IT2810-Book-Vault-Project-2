3da24a16e96797be9fe6726c7dbdcaea1fc6d6b3
Lasse Bærland Strand <lassebs@stud.ntnu.no>
2024-11-01 10:02:11 +0100

    fix: Fix errors after merge conflict

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -23,8 +23,8 @@ import styles from './Header.module.css';
 const mainLinks = [
   { link: '/', label: 'Home' },
   { link: '/books', label: 'Books' },
-  { link: '/myReviews', label: 'REVIEWS' },
-  { link: '/testing', label: 'TESTING' },
+  { link: '/myReviews', label: 'Reviews' },
+  { link: '/testing', label: 'Testing' },
 ];
 
 // Links that should show on the mobile drop down, but has an alternate display style on desktop
