15fd03f8509b6540efcdcda4bebcf63fee294371
Lasse Bærland Strand <lassebaerlandstrand@gmail.com>
2024-11-03 16:11:16 +0100

    fix: Stats is now updated when going to home page

--- frontend/src/hooks/useStats.ts
+++ frontend/src/hooks/useStats.ts
@@ -2,11 +2,12 @@ import { useQuery } from '@apollo/client';
 import { GET_STATS } from '@/graphql/queries/stats';
 
 export const useStats = () => {
-  const { data, loading, error } = useQuery(GET_STATS);
+  const { data, loading, error, refetch } = useQuery(GET_STATS);
 
   return {
     stats: data?.stats,
     loading,
     error,
+    refetch,
   };
 };
