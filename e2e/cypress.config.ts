e9e9a7a8968dd327d48aa361625a55dbe20bc5b7
Lasse Bærland Strand <lassebaerlandstrand@gmail.com>
2024-11-05 19:45:33 +0100

    feat: Add eslint to e2e

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -3,8 +3,5 @@ import { defineConfig } from 'cypress';
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173/project2',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 });
