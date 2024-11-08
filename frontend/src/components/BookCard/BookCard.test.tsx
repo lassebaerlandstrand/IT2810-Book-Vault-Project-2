211f28ac1710bf0b343fc1da76993d10eb0f1d6b
Randy <kriosen03@outlook.com>
2024-11-08 09:18:25 +0100

    test: update snapshots

--- frontend/src/components/BookCard/BookCard.test.tsx
+++ frontend/src/components/BookCard/BookCard.test.tsx
@@ -5,6 +5,16 @@ import BookCard from './BookCard';
 describe('Bookcard component', () => {
   it('matches snapshot', () => {
     const { asFragment } = render(<BookCard book={dummyBook} />);
+    const attributesToRemove = [
+      ...document.body.querySelectorAll('div [id^="mantine"]'),
+      ...document.body.querySelectorAll('div [for^="mantine"]'),
+    ]; // Because Mantine uses random ids which causes snapshots to fail
+    attributesToRemove.forEach((element) => {
+      element.removeAttribute('for');
+      element.removeAttribute('id');
+      element.removeAttribute('class');
+      element.removeAttribute('aria-describedby');
+    });
     expect(asFragment()).toMatchSnapshot();
   });
 });
