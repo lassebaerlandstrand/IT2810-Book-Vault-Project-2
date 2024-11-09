d9fa4606e728e2155d72181ce8d6a61d3e3f9e6c
Randy <kriosen03@outlook.com>
2024-11-09 20:49:10 +0100

    feat: use proper input type

--- frontend/src/hooks/updateUser.ts
+++ frontend/src/hooks/updateUser.ts
@@ -1,10 +1,11 @@
 import { useMutation } from '@apollo/client';
+import { UpdateUserInput } from '@/generated/graphql';
 import { UPDATE_USER } from '@/graphql/mutations/users';
 
 export const updateUser = () => {
   const [updateUser, { loading, error }] = useMutation(UPDATE_USER);
 
-  const submitUpdate = async (input: { name: string; UUID: string }) => {
+  const submitUpdate = async (input: UpdateUserInput) => {
     try {
       await updateUser({
         variables: { input },
