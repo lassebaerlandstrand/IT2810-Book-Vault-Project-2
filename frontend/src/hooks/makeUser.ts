cef108f3db1698a962389561bcf107be3e835699
Sebastian Munthe <sebasmu@stud.ntnu.no>
2024-11-10 16:04:13 +0100

    fix: updated env and undid change in makeUser

--- frontend/src/hooks/makeUser.ts
+++ frontend/src/hooks/makeUser.ts
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useMutation } from '@apollo/client';
 import { CREATE_USER } from '@/graphql/mutations/users';
 
@@ -11,8 +12,19 @@ import { CREATE_USER } from '@/graphql/mutations/users';
 export const makeUser = () => {
   const [createUser, { data, loading, error }] = useMutation(CREATE_USER);
 
+  useEffect(() => {
+    const createUserAsync = async () => {
+      try {
+        await createUser();
+      } catch (e) {
+        console.error('Error during user creation:', e);
+      }
+    };
+
+    createUserAsync();
+  }, [createUser]);
+
   return {
-    createUser,
     user: data?.createUser || null,
     loading,
     error,
